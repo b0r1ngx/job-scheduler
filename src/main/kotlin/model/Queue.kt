6fe25de469ab690bb27b06978076332fcafe760c
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-04-19 01:04:52 +0300

    Created own-implemented Queue

---
+++ src/main/kotlin/model/Queue.kt
@@ -0,0 +1,34 @@
+package model
+
+import model.task.Priority
+import model.task.Task
+import java.util.LinkedList
+import java.util.NoSuchElementException
+
+class Queue {
+    private val queue = buildMap {
+        Priority.entries.forEach {
+            put(it, LinkedList<Task>())
+        }
+    }
+
+    var size: Int = 0
+
+    fun add(task: Task) {
+        queue[task._priority]?.add(task)
+        size++
+    }
+
+    fun pop(): Task {
+        if (size > 0) {
+            queue.forEach { (_, queue) ->
+                try {
+                    return queue.pop().also { size-- }
+                } catch (e: NoSuchElementException) {
+                    return@forEach
+                }
+            }
+        }
+        throw NoSuchElementException("Queue is empty")
+    }
+}
