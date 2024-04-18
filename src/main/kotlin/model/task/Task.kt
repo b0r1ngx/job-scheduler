39da314ce7d1bf1848d6de9840408c69caf6b311
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-04-19 00:12:26 +0300

    backup changes to 2-previous commits (include this) version, why: explained in WIP of previous commit

--- src/main/kotlin/model/task/Task.kt
+++ src/main/kotlin/model/task/Task.kt
@@ -6,11 +6,9 @@ class Task(
     priority: Priority,
     val name: String = UUID.randomUUID().toString(),
     val type: Type = Type.Basic
-) : Runnable, Comparable<Task> {
+) : Runnable {
     var _priority: Priority = priority
         private set
-
-    private val timestamp = System.currentTimeMillis()
 
     override fun run() {
         Thread.sleep(1)
@@ -18,9 +16,5 @@ class Task(
 
     override fun toString(): String {
         return "Task(name=$name, type=$type, priority=$_priority)"
-    }
-
-    override fun compareTo(other: Task): Int {
-        return (this.timestamp - other.timestamp).toInt()
     }
 }
