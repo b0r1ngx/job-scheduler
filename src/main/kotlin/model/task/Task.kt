021a0765da6c7760db48e848c4ea114beab91f4c
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-04-18 22:28:00 +0300

    1) add Type parameter to Task 2) Change default name "" to UUID 3) decrease run

--- src/main/kotlin/model/task/Task.kt
+++ src/main/kotlin/model/task/Task.kt
@@ -1,17 +1,20 @@
 package model.task
 
+import java.util.UUID
+
 class Task(
     priority: Priority,
-    val name: String = ""
+    val type: Type = Type.Basic,
+    val name: String = UUID.randomUUID().toString()
 ) : Runnable {
     var _priority: Priority = priority
         private set
 
     override fun run() {
-        Thread.sleep(100)
+        Thread.sleep(1)
     }
 
     override fun toString(): String {
-        return "Task(priority=$_priority, name=$name)"
+        return "Task(name=$name, type=$type, priority=$_priority)"
     }
 }
