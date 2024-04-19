56dfea26b68b0449b0f9a3e6e394c99de0e71f2d
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-04-19 12:27:46 +0300

    Created parameter state for Task

--- src/main/kotlin/task/Task.kt
+++ src/main/kotlin/task/Task.kt
@@ -1,6 +1,8 @@
 package task
 
+import statemachine.State
 import java.util.UUID
+import java.util.concurrent.atomic.AtomicLong
 
 class Task(
     priority: Priority,
@@ -10,8 +12,11 @@ class Task(
     var _priority: Priority = priority
         private set
 
+    private var state = State.Ready
+
     override fun run() {
         Thread.sleep(1)
+        // after run process success -> became to next state?
     }
 
     override fun toString(): String {
