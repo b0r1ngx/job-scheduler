003f083e3f59fee5d4c085dd3c7659deae7723b3
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-04-21 14:22:14 +0300

    1) prevent infinity loop at Scheduler, with System.isTasksEnded; 2) upgrade logs.
    P.S. previous commits, without it, is statically not compiled

--- src/main/kotlin/Scheduler.kt
+++ src/main/kotlin/Scheduler.kt
@@ -1,34 +1,33 @@
 import task.Task
 
+private const val TAG = "SCHEDULER:"
+
 class Scheduler(
     val queue: Queue,
     private val processor: Processor
 ) {
     private var currentTaskOnExecution: Task? = null
 
-    fun run() {
-        checkIfQueueHasMorePrioritizedTasks()
+    fun run(isTasksEnded: () -> Boolean) {
+        checkIfQueueHasMorePrioritizedTasks(isTasksEnded)
     }
 
-    private fun checkIfQueueHasMorePrioritizedTasks() {
-        while (true) {
-            if (processor.isFree && queue.size > 0)
+    private fun checkIfQueueHasMorePrioritizedTasks(isTasksEnded: () -> Boolean) {
+        while (isTasksEnded()) {
+            if (processor.isFree && queue.size > 0) {
+                println("$TAG + start execute on processor, because its free")
                 startExecutionOnProcessor(queue.pop())
+            }
 
             val (isHigherPriorityTaskAppeared, higherPriorityTask) =
                 queue.popHigherTaskIfExists(currentTaskPriority = currentTaskOnExecution?.priority)
 
-//            println(currentTaskOnExecution)
             if (!isHigherPriorityTaskAppeared) continue
-            println("$higherPriorityTask ////_WANT TO BEAT_//// $currentTaskOnExecution")
-
-            // TODO: if it may be helpful: here we can collect info of task that was shutdown.
+            println("$TAG 1s beats 2s - 1s:$higherPriorityTask, 2s:$currentTaskOnExecution")
 
             processor.shutdownNow().also {
-                currentTaskOnExecution?.terminate().also {
-                    currentTaskOnExecution?.activate().also {
-                        queue.add(currentTaskOnExecution!!)
-                    }
+                currentTaskOnExecution?.preempt().also {
+                    queue.add(currentTaskOnExecution!!)
                 }
             }
             startExecutionOnProcessor(higherPriorityTask!!)
@@ -36,7 +35,6 @@ class Scheduler(
     }
 
     private fun startExecutionOnProcessor(task: Task) {
-        println("startExecutionOnProcessor() with Task: $task")
         currentTaskOnExecution = task
         processor.execute(task)
     }
