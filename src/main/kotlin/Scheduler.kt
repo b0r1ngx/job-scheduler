845e03fab857b4dd8311641401ea907a216fa969
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-04-21 08:34:29 +0300

    1) Add logic to start task, when processor free
    2) When want to end current task (with processor.shutdownNow(), also change statuses of Task and back it to queue

--- src/main/kotlin/Scheduler.kt
+++ src/main/kotlin/Scheduler.kt
@@ -12,25 +12,32 @@ class Scheduler(
 
     private fun checkIfQueueHasMorePrioritizedTasks() {
         while (true) {
+            if (processor.isFree && queue.size > 0)
+                startExecutionOnProcessor(queue.pop())
+
             val (isHigherPriorityTaskAppeared, higherPriorityTask) =
                 queue.popHigherTaskIfExists(currentTaskPriority = currentTaskOnExecution?.priority)
 
 //            println(currentTaskOnExecution)
             if (!isHigherPriorityTaskAppeared) continue
-            println(currentTaskOnExecution)
+            println("$higherPriorityTask ////_WANT TO BEAT_//// $currentTaskOnExecution")
 
             // TODO: if it may be helpful: here we can collect info of task that was shutdown.
-            processor.executor.shutdownNow()
+
+            processor.shutdownNow().also {
+                currentTaskOnExecution?.terminate().also {
+                    currentTaskOnExecution?.activate().also {
+                        queue.add(currentTaskOnExecution!!)
+                    }
+                }
+            }
             startExecutionOnProcessor(higherPriorityTask!!)
         }
     }
 
     private fun startExecutionOnProcessor(task: Task) {
+        println("startExecutionOnProcessor() with Task: $task")
         currentTaskOnExecution = task
-        processor.executor.execute(task)
-    }
-
-    fun addTask(task: Task) {
-        queue.add(task)
+        processor.execute(task)
     }
 }
