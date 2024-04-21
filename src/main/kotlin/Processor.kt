b13fabf74d21353d63df0e5482172e4c7b111d0f
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-04-21 09:09:26 +0300

    add logs

--- src/main/kotlin/Processor.kt
+++ src/main/kotlin/Processor.kt
@@ -2,18 +2,22 @@ import task.Task
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+private const val TAG = "PROCESSOR: "
+
 class Processor {
     private var thread: ExecutorService = Executors.newSingleThreadExecutor()
     var isFree = true
         private set
 
     fun execute(task: Task) {
+        println(TAG + "execute($task)")
         isFree = false
         thread.execute(task)
         isFree = true
     }
 
     fun shutdownNow() {
+        println(TAG + "shutdownNow()")
         thread.shutdownNow()
         thread = Executors.newSingleThreadExecutor()
         isFree = true
